08decc5092a81c3941af2bf64210379ea6ee263d
Stefan Knudsen <stefan.knudsen@live.dk>
2024-11-06 12:42:17 +0100

    Initial Commit

---
+++ Github Exercise/Github_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_ExerciseApp.swift
+//  Github Exercise
+//
+//  Created by Stefan Knudsen on 06/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct Github_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
